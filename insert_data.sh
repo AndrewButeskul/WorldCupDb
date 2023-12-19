e732a4fbdf75ed40ee436ac09d35d02086d539dc
Andrii Buteskul <buteskul5.6@gmail.com>
2023-12-19 13:02:46 -0500

    Task finished

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,22 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo "$($PSQL "TRUNCATE TABLE games, teams")"
+
+# Adding names to the table 'teams'
+while IFS="," read -r winner opponent
+do
+  Insert_winner=$($PSQL "INSERT INTO teams(name) VALUES('$winner') ON CONFLICT (name) DO NOTHING;")  
+  Inssert_opponent=$($PSQL "INSERT INTO teams(name) VALUES('$opponent') ON CONFLICT (name) DO NOTHING;")
+done < <(cut -d "," -f3,4 games.csv | tail -n +2)
+
+# Adding data to the table 'games'
+while IFS="," read -r year round winner opponent winner_goals opponent_goals
+do
+  winner_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")
+  opponent_ID=$($PSQL "SELECT team_id FROM teams WHERE name ='$opponent'")
+
+  INSERT_DATA=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($year, '$round', $winner_ID, $opponent_ID, $winner_goals, $opponent_goals);")
+done < <(tail -n +2 games.csv)
+echo "All data have been written down!"
